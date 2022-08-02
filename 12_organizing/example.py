1dff86ada7628bee36f2154f8e5fd7664e0c2a0a
Martin Spacek <git@mspacek.mm.st>
2022-08-02 12:11:05 +0200

    Add class 12 notes

---
+++ 12_organizing/example.py
@@ -0,0 +1,15 @@
+def squared(x):
+    """Return the square of x.
+
+    >>> squared(2)
+    4
+    >>> squared(3)
+    9
+    >>> squared(4)
+    15
+    """
+    return x**2
+
+
+if __name__ == '__main__':
+    print('This .py file has been run as a script')
